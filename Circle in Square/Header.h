1ca6c6ff4adba03a8a2bc6067afffa0ffc60c1a3
Maria Shevchenko <sevcenkomaria72@gmail.com>
2023-10-10 23:44:52 +0300

    Добавьте файлы проекта.

---
+++ Circle in Square/Header.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+class Circle {
+protected:
+	int radius;
+public:
+	Circle() {
+		radius = 0;
+	}
+	Circle(int r) {
+		radius = r;
+	}
+	void SetRadius(int r) {
+		radius = r;
+	}
+	int GetRadius() {
+		return radius;
+	}
+	void EnterData() {
+		cout << "Enter circle radius: ";
+		cin >> radius;
+	}
+	void Print() {
+		cout << "circle radius: " << radius << endl;
+	}
+};
+
+class Square {
+protected:
+	int side;
+public:
+	Square() {
+		side = 0;
+	}
+	void SetSide(int s) {
+		side = s;
+	}
+	Square(int s) 
+	{
+		side = s;
+	}
+	int GetSide() 
+	{
+		return side;
+	}
+	void EnterData() 
+	{
+		cout << "Enter side size: ";
+		cin >> side;
+	}
+	void Print() 
+	{
+		cout << "square side: " << side << endl;
+	}
+};
+
+class CircleInSquare :public Circle, public Square
+{
+public:
+	CircleInSquare() = default;
+	CircleInSquare(int r, int s) {
+		Circle::SetRadius(r);
+		Square::SetSide(s);
+	}
+	void Print(){
+		cout << "Circle radius: " << Circle::GetRadius() << endl;
+		cout << "Square side: " << Square::GetSide() << endl;
+		if (Circle::GetRadius() == (Square::GetSide() / 2)) {
+			cout << "Circle inscribed in a square!" << endl;
+		}
+		else {
+			cout << "Error" << endl;
+		}
+	}
+};
---
+++ Circle in Square/main.cpp
@@ -0,0 +1,8 @@
+#include "Header.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	CircleInSquare a(5,10);
+	a.Print();
+}
